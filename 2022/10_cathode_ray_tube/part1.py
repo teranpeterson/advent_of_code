1d31a3d8099cf016c312dbb4b76bb6dccdc6a676
Peterson, Teran Eric <tpeterson60@gatech.edu>
2022-12-10 11:36:14 -0700

    poof

---
+++ 2022/10_cathode_ray_tube/part1.py
@@ -0,0 +1,34 @@
+def printReg(cycles, X):
+    if cycles == 20 or cycles == 60 or cycles == 100 or cycles == 140 or cycles == 180 or cycles == 220:
+        strength = cycles*X
+        print(f"{cycles}: {X} = {strength}")
+        return strength
+    else:
+        return 0
+
+with open("input.txt", "r") as file:
+    lines = [line.rstrip() for line in file]
+
+X = 1
+cycles = 1
+sum = 0
+
+i = 0
+while True:
+    if i == len(lines):
+        break
+    line = lines[i].split()
+    inst = line[0]
+    if inst == "addx":
+        val = int(line[1])
+        sum += printReg(cycles, X)
+        cycles += 1
+        sum += printReg(cycles, X)
+        cycles += 1
+        X += val
+    else:
+        sum += printReg(cycles, X)
+        cycles += 1
+    i +=1
+sum += printReg(cycles, X)
+print(sum)
---
+++ 2022/10_cathode_ray_tube/part2.py
@@ -0,0 +1,32 @@
+def printReg(d, X):
+    cycles = d % 40
+    if X == cycles or X + 1 == cycles or X + 2 == cycles:
+        print("#", end="")
+    else:
+        print(".", end="")
+    if cycles == 0:
+        print()
+
+with open("input.txt", "r") as file:
+    lines = [line.rstrip() for line in file]
+
+X = 1
+cycles = 1
+
+i = 0
+while True:
+    if i == len(lines):
+        break
+    line = lines[i].split()
+    inst = line[0]
+    if inst == "addx":
+        val = int(line[1])
+        printReg(cycles, X)
+        cycles += 1
+        printReg(cycles, X)
+        cycles += 1
+        X += val
+    else:
+        printReg(cycles, X)
+        cycles += 1
+    i +=1
